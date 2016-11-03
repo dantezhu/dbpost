f995c68dbef15d8a1e8a47cf5806823eb9ece0e4
dantezhu <dantezhu@qq.com>
2016-11-03 14:09:46 +0800

    默认日志

--- examples/demo_config.py
+++ examples/demo_config.py
@@ -9,3 +9,44 @@ SECRET = None
 
 # 最多可以有多少个uri
 # MAX_URI_COUNT = 50
+
+
+LOG_FORMAT = '\n'.join((
+    '/' + '-' * 80,
+    '[%(levelname)s][%(asctime)s][%(process)d:%(thread)d][%(filename)s:%(lineno)d %(funcName)s]:',
+    '%(message)s',
+    '-' * 80 + '/',
+))
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    'formatters': {
+        'standard': {
+            'format': LOG_FORMAT,
+        },
+    },
+
+    'handlers': {
+        'flylog': {
+            'level': 'ERROR',
+            'class': 'flylog.LogHandler',
+            'formatter': 'standard',
+            'source': 'dbpost',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+
+    'loggers': {
+        'dbpost': {
+            'handlers': ['console', 'flylog'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    }
+}
