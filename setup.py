37f0de6ae7622466e2498721884c0acaff9fa512
dantezhu <dantezhu@qq.com>
2016-11-03 17:06:28 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="dbpost",
-    version="0.2.11",
+    version="0.2.13",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires=['pymongo', 'dataset'],
     scripts=['dbpost/bin/run_dbpost.py'],
