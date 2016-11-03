960e8bdc74b7cf0a3fa05a76fd18375488fa9fb7
dantezhu <dantezhu@qq.com>
2016-11-03 12:49:14 +0800

    继续提交

--- dbpost/bin/run_dbpost.py
+++ dbpost/bin/run_dbpost.py
@@ -7,6 +7,7 @@ eg.
 run_dbpost.py -p 35000 -s 'l%t-$72o67!hs1-^!1&ayj5uf2b39s57' -d
 """
 
+import imp
 import argparse
 
 import sys
@@ -20,11 +21,21 @@ def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--host', default=constants.SERVER_HOST, help='bind host', action='store')
     parser.add_argument('-p', '--port', default=constants.SERVER_PORT, type=int, help='bind port', action='store')
-    parser.add_argument('-s', '--secret', help='secret', action='store')
-    parser.add_argument('-m', '--max_pool_size', help='max_pool_size', type=int, action='store')
-    parser.add_argument('-c', '--max_uri_count', help='max_uri_count', type=int, action='store')
+    parser.add_argument('-c', '--config', help='config file', action='store', required=False)
     parser.add_argument('-v', '--version', action='version', version='%s' % dbpost.__version__)
     return parser
+
+
+def load_config(filename):
+    d = imp.new_module('config')
+    d.__file__ = filename
+    try:
+        with open(filename) as config_file:
+            exec(compile(config_file.read(), filename, 'exec'), d.__dict__)
+    except IOError as e:
+        e.strerror = 'Unable to load configuration file (%s)' % e.strerror
+        raise
+    return d
 
 
 def run():
@@ -34,7 +45,7 @@ def run():
         host=args.host, port=args.port)
     )
 
-    app = Server(args.secret, max_pool_size=args.max_pool_size, max_uri_count=args.max_uri_count)
+    app = Server(config=load_config(args.config) if args.config else None)
     try:
         app.run(args.host, args.port)
     except KeyboardInterrupt:
---
+++ examples/demo_config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+# 密钥
+SECRET = None
+
+# 每个uri对应的最多的client数
+# MAX_POOL_SIZE = 50
+
+# 最多可以有多少个uri
+# MAX_URI_COUNT = 50
