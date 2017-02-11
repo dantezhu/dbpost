10fa1b5f746061503a96d980743dd178bb0db883
朱念洋 <dantezhu@dantezhuMBP.lan>
2017-02-11 15:32:24 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="dbpost",
-    version="0.2.17",
+    version="0.2.19",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires=['pymongo', 'dataset'],
     scripts=['dbpost/bin/run_dbpost.py'],
